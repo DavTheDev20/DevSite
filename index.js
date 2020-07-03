49da86349bac9a0bf4269a8f701b1fbeba584580
Daxman21 <47047243+Daxman21@users.noreply.github.com>
2020-07-03 12:25:20 -0400

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-function messageConf(){
-  var name = document.querySelector('.name').value;
-  alert('Thank you, ' + name + " for your message, I will respond as soon as possible.");
-}
-
-
-// Updated 7/1/2020 7:17pm
