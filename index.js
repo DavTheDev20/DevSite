f509bc59ea606d2c458b0ccd8b57903914c0c5f4
Daxman21 <47047243+Daxman21@users.noreply.github.com>
2020-08-24 21:18:34 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 function messageConf(){
   var name = document.querySelector('.name').value;
+  //Name Correction
+  var firstLetter = name.slice(0,1);
+  var restOfName = name.slice(1, name.length);
+  var correctedName = (firstLetter.toUpperCase() + restOfName.toLowerCase());
+  //Name Correction
   var message = document.querySelector('.message').value;
   if (name === '' && message === '') {
     event.preventDefault();
     return alert('Please enter your name, email, and a brief message.');
   } else {
-    return alert('Thank you, ' + name + " for your message, I will respond as soon as possible.");
+    return alert('Thank you, ' + correctedName + " for your message, I will respond as soon as possible.");
   }
 }
 
-// Updated 7/3/2020 12:30pm
+// Updated 8/24/2020 9:00PM
